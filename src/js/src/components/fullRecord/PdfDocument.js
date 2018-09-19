b0618deb23120fc22d9e907f8fbb801fcb18371b
jt <jt@kb.dk>
2018-09-19 13:15:21 +0200

    PdfDocument first stab

--- src/js/src/components/fullRecord/PdfDocument.js
+++ src/js/src/components/fullRecord/PdfDocument.js
@@ -1,11 +1,52 @@
 export default {
   name: "pdfDocument",
 
-  data: () => ({}),
+  props: {
+    pdfUrl: {
+      type: String,
+      required: true
+    },
 
-  methods: {},
+    startPage: {
+      type: Number,
+      required: false
+    }
+  },
+
+  data: () => ({ pages: [], pdf: {}, scale: 2 }),
+
+  created() {
+    this.fetchPDF();
+  },
+
+  methods: {
+    setPdf(pdf) {
+      this.pdf = pdf;
+    },
+    setPages(pages) {
+      this.pages = pages;
+    },
+
+    fetchPDF() {
+      //On the fly importing of pdfjs
+      import("pdfjs-dist/webpack")
+        .then(pdfjs => pdfjs.getDocument(this.pdfUrl))
+        .then(pdf => this.setPdf(pdf));
+    }
+  },
 
   render(h) {
-    <div />;
+    <div class="pdfDoc">
+      <PDFPage pages={this.pages} scale={this.scale} />
+    </div>;
+  },
+
+  watch: {
+    pdf(pdf) {
+      this.pages = [];
+      const promises = range(1, pdf.numPages).map(number => pdf.getPage(number));
+
+      Promise.all(promises).then(pages => this.setPages(pages));
+    }
   }
 };
