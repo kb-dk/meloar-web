eedeaa2decb633a4fd6ddaa74a0992d2ceda4050
jt <jt@kb.dk>
2018-09-20 11:18:11 +0200

    reverted bad changes that F* things up

--- src/js/src/components/SearchResult.js
+++ src/js/src/components/SearchResult.js
@@ -12,33 +12,6 @@ export default {
     }
   },
   render(h) {
-    return (
-      <div class="searchResult">
-        <div>
-          <div class="title">Author:</div>
-          {this.result.author.map(function(name, i, arr) {
-            if (arr.length - 1 === i) {
-              return <div class="author">{name}</div>;
-            } else {
-              return <div class="author">{name},</div>;
-            }
-          })}
-        </div>
-        <div>
-          <div class="title">Chapter:</div>
-          <div class="chapter">{this.result.chapter}</div>
-        </div>
-        <div>
-          <div class="title">Content:</div>
-          <div class="content">{this.result.content[0]}</div>
-        </div>
-        <div>
-          <div class="title">Ressource:</div>
-          <div class="external_ressource">{this.result.external_resource}</div>
-        </div>
-
-        <router-link to="{this.result.id}">See more.</router-link>
-      </div>
-    );
+    return <router-link to={this.getRecordLink(this.result.id)}>See more.</router-link>;
   }
 };
