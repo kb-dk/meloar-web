1dfa7779879e5e54e4e5923782d01ba92cdc0659
jt <jt@kb.dk>
2018-09-21 14:02:48 +0200

    pdf once again

--- src/js/src/components/fullRecord/PdfDocument.js
+++ src/js/src/components/fullRecord/PdfDocument.js
@@ -1,5 +1,5 @@
-import PDFPage from "./PdfPage.js";
 import range from "lodash/range";
+import pdfjs from "pdfjs-dist";
 
 export default {
   name: "pdfDocument",
@@ -11,56 +11,70 @@ export default {
     }
   },
 
-  data: () => ({ pdf: undefined, pages: [] }),
-
-  created() {
-    this.fetchPDF();
-  },
-
   methods: {
     getPdfUrl: function() {
       console.log(this.record.doc ? this.record.doc.loar_resource[0] : "");
-      //return this.record.doc ? this.record.doc.loar_resource[0] : "";
+      //return "/api/pdf/" + (this.record.doc ? this.record.doc.external_resource[0] : "");
       return "/api/pdf";
     },
 
-    getScale: function() {
-      return 2;
+    getPage: function() {
+      return this.record.doc ? this.record.doc.page : "";
     },
+
+    showPdf: function() {},
 
     setPages: function(pages) {
       this.pages = pages;
-    },
-
-    setPdf: function(pdf) {
-      this.pdf = pdf;
-    },
-
-    fetchPDF() {
-      //On the fly importing of pdfjs
-      import("pdfjs-dist/webpack")
-        .then(pdfjs => pdfjs.getDocument(this.getPdfUrl()))
-        .then(pdf => this.setPdf(pdf));
     }
   },
 
   render(h) {
-    return (
-      <div class="pdfDoc">
-        {this.pages.map(page => (
-          <PDFPage page={page} scale={this.scale} />
-        ))}
-      </div>
-    );
-  },
-
-  watch: {
-    pdf(pdf) {
-      this.pages = [];
-      const promises = range(1, pdf.numPages).map(number => pdf.getPage(number));
-
-      Promise.all(promises).then(pages => this.setPages(pages));
-      console.log("all pdf pages set!!!");
-    }
+    return <div class="pdf">{renderPdf(this.getPage(), this.getPdfUrl())}</div>;
   }
 };
+
+function renderPdf(pageNumber, pdfUrl) {
+  // If absolute URL from the remote server is provided, configure the CORS
+  // header on that server.
+  //var url = "//cdn.mozilla.net/pdfjs/helloworld.pdf";
+  var url = "/api/pdf";
+
+  // Asynchronous download of PDF
+  var loadingTask = pdfjs.getDocument(url);
+  loadingTask.promise.then(
+    function(pdf) {
+      console.log("PDF loaded");
+
+      // Fetch the first page
+      var pageNumber = 1;
+      pdf.getPage(pageNumber).then(function(page) {
+        console.log("Page loaded");
+
+        var scale = 1.5;
+        var viewport = page.getViewport(scale);
+
+        // Prepare canvas using PDF page dimensions
+        var canvas = document.createElement("canvas");
+        var context = canvas.getContext("2d");
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        // Render PDF page into canvas context
+        var renderContext = {
+          canvasContext: context,
+          viewport: viewport
+        };
+        var renderTask = page.render(renderContext);
+        renderTask.then(function() {
+          console.log("Page rendered");
+          document.body.appendChild(canvas);
+        });
+      });
+    },
+    function(reason) {
+      // PDF loading error
+      console.error(reason);
+    }
+  );
+}
